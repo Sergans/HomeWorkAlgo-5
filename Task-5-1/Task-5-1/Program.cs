549cd490511a529fc0fe0de84ab7cdf7bff2f45c
Сергей <gan7er@yandex.ru>
2021-05-24 18:35:51 +0300

    Начало,дерево

---
+++ Task-5-1/Task-5-1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task_5_1
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode RightChild { get; set; }
+        public TreeNode LefttChild { get; set; }
+        public TreeNode Parent { get; set; }
+        public TreeNode Root { get; set; }
+
+        public void AddItem(int value)
+        {
+
+            if (Root == null)
+            {
+                TreeNode root = new TreeNode { Value = value };
+                Root = root;
+            }
+           
+            else
+            {
+                Add(Root, value);
+            }
+
+        }
+        public void Add(TreeNode node, int value)
+        {
+            TreeNode next = new TreeNode { Value = value };
+            if (node.Value > value)
+            {
+                
+                if (node.LefttChild == null)
+                {
+                    node.LefttChild = next;
+                    next.Parent = node;
+                }
+                else
+                {
+                    
+                    Add(node.LefttChild, value);
+                }
+            }
+            else
+            {
+                if (node.RightChild == null)
+                {
+                    node.RightChild = next;
+                    next.Parent = node;
+                }
+                else
+                {
+                    
+                    Add(node.RightChild, value);
+                }
+
+
+            }
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
